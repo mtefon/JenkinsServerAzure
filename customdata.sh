07e5b6dfa21c3ed0aa389b8ba343816d83b38614
Yasin Cetindil <ycetindil@gmail.com>
2023-03-08 21:30:59 -0800

    Update customdata.sh

--- customdata.sh
+++ customdata.sh
@@ -49,3 +49,7 @@ gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 apt-get update
 apt-get install terraform
+
+# Install Kubectl
+curl https://s3.us-west-2.amazonaws.com/amazon-eks/1.24.10/2023-01-30/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl
+chmod +x /usr/local/bin/kubectl
